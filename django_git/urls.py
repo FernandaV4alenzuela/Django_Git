ebf46df7bb1503af3378b52ab773f5c2ff219ce4
Fnlok <desertnlol@gmail.com>
2024-08-30 10:19:07 -0400

    app3 terminada

--- django_git/urls.py
+++ django_git/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path, include 
 from app1 import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include("app1.urls")),
     path('app2/',include("app2.urls")),
+    path('app3/',include("app3.urls")),
+
+    
 ]
